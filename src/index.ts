e7b8d9c799c0e7236d74d726fb7ecf303290aa72
Thiwanka-Sandakalum <thiwanka2002sandakalum@gmail.com.com>
2024-06-21 17:14:12 +0530

    update opanapi

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import router from './router.';
 import { PrismaClient } from '@prisma/client';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const prisma = new PrismaClient();
 
 // Middleware
